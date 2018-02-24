f6d4ffd83713ed162dc996971f9d70b773a4d1a3
Omar Guerrero <guerrero9725@gmail.com>
2018-02-24 15:57:48 -0500

    feat: First version of my cli

---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+'use strict';
+
+const commander = require('commander')
+const fs = require('fs')
+
+function handleCreate (name, cmd) {
+    let path = cmd.path || './src/components',
+        pascal = pascalize(name)
+
+    let template = `import React, {Component} from 'react'
+
+class ${pascal} extends Component {
+    constructor (props) {
+        super(props)
+    }
+
+    render () {
+        return <h1>New component ${pascal}</h1>
+    }
+}
+
+export default ${pascal}
+`
+
+    writeReactComponent(name, path, template)
+}
+
+function writeReactComponent (name, path, data) {
+    let url = `${path}/${smallCaps(name)}.js`
+
+    fs.writeFile(url, data, function (err) {
+        if (err) throw console.log(err)
+        console.log('New component created!')
+    })
+}
+
+function pascalize (str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+        if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+        return match.toUpperCase();
+    });
+}
+
+function smallCaps (str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+        if (+match === 0) return "_"; // or if (/\s+/.test(match)) for white spaces
+        return match.toLowerCase();
+    });
+}
+
+commander
+    .version('1.0.0', '-v', '--version')
+    .description('My own CLI app')
+
+commander
+    .command('crc <name>')
+    .option('-p, --path [path]', 'Change path of component')
+    .action(handleCreate)
+
+commander.parse(process.argv)
