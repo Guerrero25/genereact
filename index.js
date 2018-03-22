5628219bec4e9f02a6cc959935bc18df2f00c14e
Omar Guerrero <guerrero9725@gmail.com>
2018-03-21 23:11:46 -0500

    feat: Added lib folder and String handler module

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 
 const commander = require('commander')
 const fs = require('fs')
+const str = require('./lib/string')
 
 const pkg = require('./package.json');
 
 function handleCreate (name, cmd) {
     let path = cmd.path || './src/components',
-        pascal = pascalize(name)
+        pascal = str.pascalize(name)
 
     let template = `import React, {Component} from 'react'\n\n` +
     `class ${pascal} extends Component {\n\t` +
@@ -25,26 +26,12 @@ function handleCreate (name, cmd) {
 }
 
 function writeReactComponent (name, path, data) {
-    let url = `${path}/${smallCaps(name)}.js`
+    let url = `${path}/${str.smallCaps(name)}.js`
 
     fs.writeFile(url, data, function (err) {
         if (err) throw console.log(err)
         console.log('New component created!')
     })
-}
-
-function pascalize (str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-        if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
-        return match.toUpperCase();
-    });
-}
-
-function smallCaps (str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-        if (+match === 0) return "_"; // or if (/\s+/.test(match)) for white spaces
-        return match.toLowerCase();
-    });
 }
 
 commander
