3835a32a5cb11bbf88a80cd0bd3344249be4ff72
Omar Guerrero <guerrero9725@gmail.com>
2018-03-24 13:36:03 -0500

    feat: Added string-template

--- index.js
+++ index.js
@@ -5,29 +5,23 @@ const commander = require('commander')
 const fs = require('fs')
 const str = require('./lib/string')
 const config = require('./lib/config')
+const templates = require('./lib/template');
 
 const pkg = require('./package.json');
 
 function handleCreate (name, cmd) {
     let path = config.getPathComponents(cmd.path, name),
-        pascal = str.pascalize(name)    
+        data = {
+            pascal_name: str.pascalize(name)
+        }
 
-    let template = `import React, {Component} from 'react'\n\n` +
-    `class ${pascal} extends Component {\n\t` +
-        `constructor (props) {\n\t` +
-            '\tsuper(props)\n' +
-        '\t}\n\n' +
-        '\trender () {\n\t' +
-            `\treturn <h1>New component ${pascal} </h1>\n` +
-        '\t}\n\n' +
-    '}\n\n' +
-    `export default ${pascal}`
+    let template = templates.getCompileTemplate(data)
 
     writeReactComponent(path, template)
 }
 
 function writeReactComponent (path, data) {
-    fs.writeFile(url, data, function (err) {
+    fs.writeFile(path, data, function (err) {
         if (err) throw console.log(err)
         console.log('New component created!')
     })
