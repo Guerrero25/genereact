3c521075654346a7c81f82f416be8f7905b7592e
Omar Guerrero <guerrero9725@gmail.com>
2018-03-30 23:02:01 -0500

    refactor: Updated unit test

--- test/components.js
+++ test/components.js
@@ -4,11 +4,11 @@ const assert = require('assert')
 describe('Create one component', () => {
     describe('getPathComponent', () => {
         it('return ./src/components when the value of path is not present', () => {
-            assert.equal(config.getPathComponents('', 'test-component'), './src/components/test_component.js')
+            assert.equal(config.getPathComponents('', 'test-component'), process.cwd() + '/src/components/test_component.js')
         })
 
         it('return component path when is present', () => {
-            assert.equal(config.getPathComponents('./src/components/test', 'test-component'), './src/components/test/test_component.js')
+            assert.equal(config.getPathComponents('src/components/test/', 'test-component'), process.cwd() + '/src/components/test/test_component.js')
         })
     })
 })
